8a80fbab559bc949aefc5d31036a6ea2b264e022
kwong3672 <keith.wong3672@gmail.com>
2016-07-08 17:11:03 -0700

    Made link controller so that it receives data and passes it into $scope

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,11 @@
-angular.module('shortly.links', [])
+angular.module('shortly.links', ['shortly.services'])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  // Links.getAll();
+  $scope.data = {};
+  Links.getAll().then(function(data) {
+    console.log('#####################################', data);
+    $scope.data.links = data;
+  });
 });
